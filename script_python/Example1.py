4e5f3a7e1a82d45cbf24bcfbab6ab085e7011d0e
fernando <fernandovillotaeraso@gmail.com>
2024-08-23 21:01:24 -0500

    script_python1

---
+++ script_python/Example1.py
@@ -0,0 +1,13 @@
+#este es un comentario de lihgjhnea
+
+'''
+Esto es un cmentario de hjgjghjgbloque
+'''
+
+n1=10
+n2=20
+suma = n1 + n2
+print (suma)
+print("la suma es: ", suma)
+print(f"la suma es: {suma}")
+print("la suma es: ",n1+n2)
