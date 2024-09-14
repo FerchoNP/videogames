391f492b93d93b169339acdfa14641e32ac10815
fernando <fernandovillotaeraso@gmail.com>
2024-09-13 21:40:46 -0500

    videogames

---
+++ script_python/number_raice/main.py
@@ -0,0 +1,11 @@
+'''
+script description Number race
+dev:Fernando  Vi
+date: 13/09/2024
+'''
+from random import randint 
+dice1 = randint(1,6)
+dice2 = randint(1,6)
+
+print (f"Dice1: {dice1} ")
+print (f"Dice2: {dice2} ")
